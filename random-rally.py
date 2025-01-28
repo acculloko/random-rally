e2f255bdca9ef14fa716bf4d117d9ed82fd6ce21
Lucca Lucchin de Campos Costa <lucca.lucchin@gmail.com>
2025-01-28 16:23:01 -0300

    const

--- random-rally.py
+++ random-rally.py
@@ -7,3 +7,6 @@ STAGES_GERMANY = ['hockweiler', 'franzenheim', 'holzerath', 'farschweiler', 'mer
 STAGES_KENYA = ['mount kenya', 'karura', 'homa bay', 'ndere island', 'lake baringo', 'lake nakuru']
 STAGES_INDONESIA = ['mount kawi', 'semangka bay', 'satonda island', 'oreng valley', 'sangeang island', 'kalabakan valley']
 STAGES_AUSTRALIA = ['gum scrub', 'toorooka', 'nulla nulla', 'comara canyon', 'lake lucernia', 'wombamurra']
+
+# next:
+# decide between OOP and simple lists, OOP would probably be better for more stats and options.
